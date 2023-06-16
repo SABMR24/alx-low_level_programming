299a2fb96581fe66ea1eb1f0920a867953a235a1
sabmr24 <saadiabouz478@gmail.com>
2023-06-15 19:08:09 -0700

    c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <studio.h>
+/**
+ * main - print the string in the put function
+ *
+ * description : using the main fuction
+ * this program  prints "Programming is like building a multilingual puzzle
+ * return 0;
+ * int main(void)
+ * {
+ *puts("\"programming is like building a multilingual puzzle");
+ return(0);
+
+ * } 
