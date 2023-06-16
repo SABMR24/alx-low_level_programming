327b71a55e543d3722f42b531ed9ebeeb878c3f9
sabmr24 <saadiabouz478@gmail.com>
2023-06-15 19:11:04 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,12 @@
  * description : using the main fuction
  * this program  prints "Programming is like building a multilingual puzzle
  * return 0;
- * int main(void)
- * {
- *puts("\"programming is like building a multilingual puzzle");
+ */
+int main(void)
+  {
+ 
+	  
+	puts("\"programming is like building a multilingual puzzle");
  return(0);
 
- * } 
+  } 
