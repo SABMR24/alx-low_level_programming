3fc2e738d1a85d8817c231d6b750ea937dbfe200
sabmr24 <saadiabouz478@gmail.com>
2023-06-17 17:19:18 -0700

    var

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a = '0';
+	int b = '1';
+
+	while (a <= '8')
+	{
+		b = a + 1;
+		while (b <= '9')
+		{
+			putchar(a);
+			putchar(b);
+			if (a != '8')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			b++;
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
