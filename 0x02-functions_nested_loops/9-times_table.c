0e4903f6982e3aad54e4ca00653220bb89d8a93b
sabmr24 <saadiabouz478@gmail.com>
2023-06-20 19:23:21 -0700

    func

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,21 +8,21 @@ void times_table(void)
 {
 	int a, b, c;
 
-	for (a = 0; a <= 9; a++)
+	for (a = 0; a <= 10; a++)
 	{
 		_putchar(48);
-		for (b = 1; b <= 9; b++)
+		for (b = 1; b <= 10; b++)
 		{
 			_putchar('.');
 			_putchar(' ');
 
 			c = a * b;
 
-			if (c <= 9)
+			if (c <= 10)
 				_putchar(' ');
 			else
 				_putchar((c /10) + 48);
-			_putchar((c % 10) + 48);
+			_putchar ((c % 10) + 48);
 		}
 		_putchar('\n');
 	}
