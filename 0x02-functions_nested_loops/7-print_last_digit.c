ee521c1a44cd4d2cf7f3a18a3bd48f8b0d390bf7
sabmr24 <saadiabouz478@gmail.com>
2023-06-20 18:39:25 -0700

    func

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,20 +2,19 @@
 
 /**
  * print_last_digit - print last digit of a number
- * @c: the int the extra digit from
- * Return: the value of last digit
+ * @n: the int the extra digit from
+ * Return: last digit
  */
 
 int print_last_digit(int n)
 {
 	int last_digit;
-	
+
 	if (n < 0)
 	last_digit = -1;
-	
 	last_digit = n % 10;
-	
-	if(last_digit < 0)
+
+	if (last_digit < 0)
 	last_digit = -last_digit;
 
 	_putchar(last_digit + '0');
