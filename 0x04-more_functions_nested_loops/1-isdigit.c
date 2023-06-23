460b5fa52c58c03a1303104432fb899d0e873f35
sabmr24 <saadiabouz478@gmail.com>
2023-06-23 05:43:14 -0700

    more func

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- *mul - multiplies two ints.
- *@i: integer.
- *@j: integer.
+ *_isdigit - check if a characterr is a digit
+ *@i: the number to be cheked
  *
- *Return: product.
+ *Return: 1 for a character that will be digit or 0 for any else
  */
 
 int _isdigit(int i)
