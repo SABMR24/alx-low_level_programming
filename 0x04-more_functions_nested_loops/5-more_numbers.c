1be6427cc94897d6324fd775cee98071d40039fa
sabmr24 <saadiabouz478@gmail.com>
2023-06-23 09:38:56 -0700

    more func

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers between 0 to 14
+ * 10 times.
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+	int i, j, k;
+
+	for (j = 0; j <= 10; j++)
+	{
+		for (k = 0; k <= 14; k++)
+		{
+			i = k;
+			if (k > 9)
+			{
+				_putchar(1 + 48);
+				i = k % 10;
+			}
+			_putchar(i + 48);
+		}
+		_putchar('\n');
+	}
+}
