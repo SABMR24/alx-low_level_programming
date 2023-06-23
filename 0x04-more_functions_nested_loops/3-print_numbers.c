df725a1e93e2bc646eaf0abc8f4306a914eb3517
sabmr24 <saadiabouz478@gmail.com>
2023-06-23 07:08:18 -0700

    more func

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0-9
+ *
+ * return: void
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0, n <= 9, n++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n')
+}
