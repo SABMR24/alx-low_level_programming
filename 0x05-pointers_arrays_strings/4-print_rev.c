faa8d2b203eeeefa2069a291088dbdebde003708
sabmr24 <saadiabouz478@gmail.com>
2023-06-26 18:45:05 -0700

    pas

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - prints reversed string, followed by a new line
+ * @s: pointer to the string to print
+ * return: void
+ */
+
+void print_rev(char *s)
+{
+	int a = 0;
+
+	while (s[a])
+		a++;
+
+	while (a--)
+	{
+		_putchar(s[a]);
+	}
+	_putchar('\n');
+}
