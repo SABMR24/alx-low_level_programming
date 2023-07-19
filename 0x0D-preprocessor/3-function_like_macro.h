65408021e9a796e60625a05f7f491d176fd61956
sabmr24 <saadiabouz478@gmail.com>
2023-07-18 17:15:00 -0700

    pro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,19 +1,6 @@
-#include <stdio.h>
-#include "3-function_like_macro.h"
-#include "3-function_like_macro.h"
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int i;
-    int j;
+#define ABS(x) ((x) < (0) ? -(x) : (x))
 
-    i = ABS(-98) * 10;
-    j = ABS(98) * 10;
-    printf("%d, %d\n", i, j);
-    return (0);
-}
+#endif
